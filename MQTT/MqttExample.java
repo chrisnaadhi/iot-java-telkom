b6a7f03f660afe717e8ab0ccdcd2ca224c1b4de8
chrisnaadhi <chrisnaadhip@gmail.com>
2025-06-05 01:39:33 +0700

    Update MqttExample.java

--- MQTT/MqttExample.java
+++ MQTT/MqttExample.java
@@ -1,36 +1,38 @@
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.eclipse.paho.client.mqttv3.*;
 
 public class MqttExample {
   public static void main(String[] args) {
     String topic = "PX Temperature and Humidity";
     String content = "T=30C and RH=40%";
     int qos = 2;
-    String broker = "tcp://mqtt.eclipseprojects.io:1883";
-    String clientId = "JavaMQTTKelompok5";
-
+    String broker = "tcp://192.168.1.17:1883";
+    String clientId = "JavaMQTTKelompok5_" + System.currentTimeMillis();
     MemoryPersistence persistence = new MemoryPersistence();
 
     try {
       MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
       MqttConnectOptions connOpts = new MqttConnectOptions();
       connOpts.setCleanSession(true);
+
       System.out.println("Connecting to broker: " + broker);
       sampleClient.connect(connOpts);
       System.out.println("Connected");
-      System.out.println("Publishing message: " + content);
+
       MqttMessage message = new MqttMessage(content.getBytes());
       message.setQos(qos);
+      message.setRetained(true); // Important for late subscribers
+
+      System.out.println("Publishing message: " + content);
       sampleClient.publish(topic, message);
       System.out.println("Message published");
+
+      Thread.sleep(1000); // Let the broker process the message
+
       sampleClient.disconnect();
       System.out.println("Disconnected");
-      System.exit(0);
-    } catch (MqttException me) {
-      me.printStackTrace();
+
+    } catch (MqttException | InterruptedException e) {
+      e.printStackTrace();
     }
   }
 }
