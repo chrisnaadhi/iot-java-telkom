97331f4b9ba5611b2b0e77dfb55dfc5c1a4df974
chrisnaadhi <chrisnaadhip@gmail.com>
2025-06-05 01:07:15 +0700

    fix deps

--- Restful-API/restcall.sh
+++ Restful-API/restcall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-javac -classpath "unirest-java-1.4.9.jar" RESTCall.java
-sudo java -classpath ".:unirest-java-1.4.9.jar" RESTCall
+javac -classpath .:unirest-java-3.13.11-standalone.jar RESTCall.java
+java -classpath .:unirest-java-1.4.9.jar RESTCall
